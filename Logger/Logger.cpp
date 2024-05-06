38008f877dbea030b401e2499ea4cda1f216c531
Tatyana Guzovskaya <tkguzovskaya@edu.hse.ru>
2024-05-06 15:46:28 +0300

    commit

---
+++ Logger/Logger.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Logger.h"
+
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    const char* s = "abracadabra";
+
+    Logger::Info(s, n);
+}
